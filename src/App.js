25509c07800e95a12c97098aa4bda8004d56cd97
ggamb <glenngambardella372@gmail.com>
2021-12-15 11:31:16 -0500

    added App.js imports and more basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {React, useState} from 'react'
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Project from './components/Project';
+import Contact from './components/Contact';
+import Footer from './components/Footer'
 
 function App() {
+  const [categories] = useState([
+    {name: 'About me'},
+    {name: 'Portfolio'},
+    {name: 'Contact'},
+    {name: 'Resume'}
+  ]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Navigation
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Navigation>
+      <main>
+        {!contactSelected ? (
+          <>
+            <About></About>
+            <Project></Project>
+          </>
+        ) : (
+          <Contact></Contact>
+        )}
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
