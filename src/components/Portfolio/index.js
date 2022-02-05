b67b6b10c5a44568cf9ee7eaaa6f9d4c55572c0c
ggamb <glenngambardella372@gmail.com>
2022-02-05 14:05:19 -0500

    updated portfolio descriptions and resume

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -18,7 +18,8 @@ function Project() {
                             <div className='portfolio-text'>
                                 <a href='https://github.com/ggamb/team-cuisine-project-3' target="_blank"><img src={github} className='small-Github' /></a>
                                 <a href="https://get-fed-project.herokuapp.com/" target="_blank" className='project-link-text'><h3>Get Fed</h3></a>
-                                <h4>An Uber Eats clone to find food near you!</h4>
+                                <h4>An Uber Eats clone to find restaurants and menus near you!</h4>
+                                <h5>Tech Stack: MERN Stack</h5>
                             </div>
                         </div>
                     </div>
@@ -29,6 +30,7 @@ function Project() {
                                 <a href='https://github.com/ggamb/Beer-belly-developers-project-2' target="_blank"><img src={github} className='small-Github' /></a>
                                 <a href="https://beer-belly.herokuapp.com/" target="_blank" className='project-link-text'><h3>Beer-Belly Developers</h3></a>
                                 <h4>Come find your favorite breweries and bars!</h4>
+                                <h5>Tech Stack: Handlebars.js, Express.js, MySQL</h5>
                             </div>
                         </div>
                     </div>
@@ -39,6 +41,7 @@ function Project() {
                                 <a href='https://github.com/ggamb/T-B-D' target="_blank"><img src={github} className='small-Github' /></a>
                                 <a href="https://evalecillos.github.io/T-B-D/" target="_blank" className='project-link-text'><h3>TunesBeDope (TBD)</h3></a>
                                 <h4>Find your favorite videos and lyrics!</h4>
+                                <h5>Tech Stack: JavaScript, HTML, CSS</h5>
                             </div>
                         </div>
                     </div>
@@ -48,7 +51,8 @@ function Project() {
                             <div className='portfolio-text'>
                                 <a href='https://github.com/ggamb/photo-port' target="_blank"><img src={github} className='small-Github' /></a>
                                 <a href="https://ggamb.github.io/photo-port/" target="_blank" className='project-link-text'><h3>React Photo Port</h3></a>
-                                <h4>An example React photo portfolio</h4>
+                                <h4>An example React photo portfolio that showcases my work in React</h4>
+                                <h5>Tech Stack: Entirely in React</h5>
                             </div>
                         </div>
                     </div>
@@ -59,6 +63,7 @@ function Project() {
                                 <a href='https://github.com/ggamb/weather-dashboard-challenge-6' target="_blank"><img src={github} className='small-Github' /></a>
                                 <a href="https://ggamb.github.io/weather-dashboard-challenge-6/" target="_blank" className='project-link-text'><h3>Weather Dashboard</h3></a>
                                 <h4>Search for weather by city</h4>
+                                <h5>Tech Stack: Entirely in JavaScript, HTML, CSS</h5>
                             </div>
                         </div>
                     </div>
