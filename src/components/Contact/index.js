8b4f2fa6104c4e81d62c1d28abd29099645bd38b
ggamb <glenngambardella372@gmail.com>
2022-02-08 16:24:23 -0500

    added text to contact form to show whether message sent or has invalid data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,9 +8,13 @@ function Contact() {
     const templateID = 'template_2kmd407';
     const userID = 'user_zp4X6Lct2JtUs0hJgaU48';
 
-    const [errorMessage, setErrorMessage] = useState('');
+    const [errorMessageEmail, setErrorMessageEmail] = useState('');
 
     const [messageSent, setMessageSent] = useState(false);
+
+    const [errorMessageName, setErrorMessageName] = useState('');
+
+    const [errorMessageText, setErrorMessageText] = useState('');
 
     const [toSend, setToSend] = useState({
         from_name: '',
@@ -20,6 +24,7 @@ function Contact() {
 
     const onSubmit = (e) => {
         e.preventDefault();
+
         send(
             serviceID,
             templateID,
@@ -27,14 +32,14 @@ function Contact() {
             userID
         )
             .then((response) => {
-                
+                //Sends email via emailjs
                 setToSend({
                     from_name: "",
                     message: "",
                     reply_to: ""
                 })
 
-                alert("Email sent successfully!");
+                //Sets messageSent to true to display text
                 setMessageSent(true);
             })
             .catch((err) => {
@@ -42,18 +47,50 @@ function Contact() {
             });
     };
 
-    
+
     const handleChange = (e) => {
         setToSend({ ...toSend, [e.target.name]: e.target.value });
     };
+
+    const handleBlur = (e) => {
+        //Handles email blur
+        if (e.target.name === 'reply_to' || e.target.length < 1) {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessageEmail('Your email is invalid.');
+            } else {
+                setErrorMessageEmail('');
+            }
+        }
+
+        //Handles name blur
+        if (e.target.name === 'from_name') {
+            if (e.target.value.length < 1) {
+                setErrorMessageName('Your name is invalid. Please let me know who to contact :)');
+            } else {
+                setErrorMessageName('');
+            }
+        }
+
+        //Handles message blur
+        if (e.target.name === 'message') {
+            if (e.target.value.length < 1) {
+                setErrorMessageName('Send me something, anything!');
+            } else {
+                setErrorMessageName('');
+            }
+
+
+        }
+    }
 
     return (
         <>
 
             <h1 data-testid="h1tag" className='tab-text'>Contact me</h1>
             <br></br>
-            <Typography variant='h4' style={{ textAlign: 'left' }}>
-                Use this form below or email me directly at:<br></br>
+            <Typography variant='h5' style={{ textAlign: 'left', paddingBottom: '10px' }}>
+                Use the form below or email me directly at:
                 <a href='mailto: glenngambardella372@gmail.com'>glenngambardella372@gmail.com</a>
             </Typography>
 
@@ -68,38 +105,61 @@ function Contact() {
                             placeholder='Your name'
                             value={toSend.from_name}
                             onChange={handleChange}
+                            onBlur={handleBlur}
+                            className='input-style'
                         />
                     </div>
                     <div>
-                        <label htmlFor="message">Message: </label>
-                        <textarea
-                            type='text'
-                            rows={5}
-                            name='message'
-                            placeholder='Your message'
-                            value={toSend.message}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="reply_to">Your email: </label>
+                        <label htmlFor="reply_to">Email: </label><br />
                         <input
-                            type='text'
+                            type='email'
                             name='reply_to'
                             placeholder='Your email'
                             value={toSend.reply_to}
                             onChange={handleChange}
+                            className='input-style'
+                            onBlur={handleBlur}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message: </label><br />
+                        <textarea
+                            type='text'
+                            name='message'
+                            placeholder='Your message'
+                            value={toSend.message}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className='message-box'
                         />
                     </div>
                     <br></br>
                     <button type='submit'>Submit</button>
                 </form>
 
+                {errorMessageEmail && (
+                    <div>
+                        <p className="error-text">{errorMessageEmail}</p>
+                    </div>
+                )}
+
+                {errorMessageName && (
+                    <div>
+                        <p className="error-text">{errorMessageName}</p>
+                    </div>
+                )}
+
+                {errorMessageText && (
+                    <div>
+                        <p className="error-text">{errorMessageText}</p>
+                    </div>
+                )}
+
                 {messageSent && (
-                        <div>
-                           <span className='check-img' role='check-img'>✅</span><p className='success-text'>Message sent successfully! I will reach out to you shortly!</p>
-                        </div>
-                    )}
+                    <div>
+                        <span className='check-img' role='check-img'>✅</span><p className='success-text'>Message sent successfully! I will reach out to you shortly!</p>
+                    </div>
+                )}
             </section>
         </>
     );
